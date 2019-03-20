85a24e8a329bc4ce7fb26ccc96af0e48becb78e0
Genes Luna <genesluna@gmail.com>
2019-03-20 09:45:07 -0300

    First commit

---
+++ DailyCodeProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DailyCodeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
