3f1c32a32352bdda12b1188a21b22f3598e96409
Genes Luna <genesluna@gmail.com>
2019-03-21 16:43:31 -0300

    fix: performance.
    went from O(n^2) to O(n)

--- DailyCodeProblems.CSharp/D01/Solution.cs
+++ DailyCodeProblems.CSharp/D01/Solution.cs
@@ -7,14 +7,36 @@ namespace DailyCodeProblems.D01
 {
     public static class Solution
     {
+        // unperformant version O(n^2)
         public static bool Run(List<int> numberlist, int k)
         {  
             foreach (var number in numberlist)
             {
+                // list search is O(n)
                 if (numberlist.Any(x => x == Math.Abs(number - k)))
                 {
                     return true;
                 }
+
+            }
+
+            return false;
+        }
+
+        //More peformant version O(n)
+        public static bool RunV2(List<int> numberlist, int k)
+        {
+            var knownNumers = new HashSet<int>();
+
+            foreach (var number in numberlist)
+            {   // hash sets search is O(1)  
+                if (knownNumers.Contains(k-number))
+                {
+                    return true;
+                }
+
+                knownNumers.Add(number);
+
             }
 
             return false;
--- DailyCodeProblems.Python/D01/Solution.py
+++ DailyCodeProblems.Python/D01/Solution.py
@@ -1,10 +1,21 @@
-from math import fabs
-
+# Unperformant version O(n^2)
 def run(numbers_list, k):
 
     for number in numbers_list:
-        if fabs(k - number) in numbers_list:
+        if k - number in numbers_list:
             return True
+
+    return False
+
+# More peformant version O(n)
+def run_v2(numbers_list, k):
+    known_numbers = set()
+
+    for number in numbers_list:
+        # sets search is O(1)
+        if k - number in known_numbers:
+            return True
+        known_numbers.add(number)
 
     return False
 
